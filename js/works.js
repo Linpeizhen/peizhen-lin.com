e7bef914f07b0d1c453ee5d10c07fef3371f3602
Linpeizhen <peizhen.lin@kkday.com>
2020-09-30 02:45:22 +0800

    Add pages

--- js/works.js
+++ js/works.js
@@ -1,12 +1,20 @@
 $(function() {
-  var workCarousel = $('.work-carousel');
+  var workCarousel = $('.work-carousel:not(.more-works-carousel)');
   workCarousel.owlCarousel({
       items: 1,
       nav: true,
       margin: 8,
-      // slideTransition: 'none',
       smartSpeed: 50,
       animateOut: 'fadeOut',
       navText: ['<i class="icon-arrow-left"></i>', '<i class="icon-arrow-right"></i>']
+  });
+
+  $('.more-works-carousel').owlCarousel({
+    items: 3,
+    nav: true,
+    slideBy: 'page',
+    margin: 30,
+    smartSpeed: 50,
+    navText: ['<i class="icon-arrow-left"></i>', '<i class="icon-arrow-right"></i>']
   });
 });
